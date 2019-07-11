9df11ef3043fad139f18955ccaee1347687ba00c
Jose David Vargas <josed1608@gmail.com>
2019-07-11 13:37:04 -0600

    Clase usuario inicial con atributos

--- src/main/java/com/tropicalizacion/tropicalizacionbackend/entidades/Usuario.java
+++ src/main/java/com/tropicalizacion/tropicalizacionbackend/entidades/Usuario.java
@@ -1,7 +1,26 @@
 package com.tropicalizacion.tropicalizacionbackend.entidades;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Column
+    @Id
+    private String correo;
+
+    @Column
+    private String contrasenna;
+
+    @Column
+    private String telefono;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private String apellidos;
 }
