42c2656508a5a14ed5c7ad67cd87a0badda3928b
Jose David Vargas <josed1608@gmail.com>
2019-07-11 13:26:38 -0600

    Creación del proyecto base

---
+++ src/main/java/com/tropicalizacion/tropicalizacionbackend/TropicalizacionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tropicalizacion.tropicalizacionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TropicalizacionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TropicalizacionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tropicalizacion/tropicalizacionbackend/TropicalizacionBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tropicalizacion.tropicalizacionbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TropicalizacionBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
