3a95eb9d1f56459faf6e9fc624994ca3fe70a512
Jose David Vargas <josed1608@gmail.com>
2019-09-30 13:50:48 -0600

    Fixes a endpoints de categorias

--- src/main/java/com/tropicalizacion/tropicalizacionbackend/controladores/CategoriaControlador.java
+++ src/main/java/com/tropicalizacion/tropicalizacionbackend/controladores/CategoriaControlador.java
@@ -2,7 +2,9 @@ package com.tropicalizacion.tropicalizacionbackend.controladores;
 
 import com.tropicalizacion.tropicalizacionbackend.entidades.CustomResponse;
 import com.tropicalizacion.tropicalizacionbackend.entidades.bd.CategoriaEntidad;
+import com.tropicalizacion.tropicalizacionbackend.entidades.dtos.CategoriaDto;
 import com.tropicalizacion.tropicalizacionbackend.servicios.CategoriaServicioImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,15 +25,17 @@ import java.util.ArrayList;
 public class CategoriaControlador {
 
     private CategoriaServicioImpl categoriaServicio;
+    private ModelMapper modelMapper;
 
     @Autowired
-    public CategoriaControlador(CategoriaServicioImpl categoriaServicio){
+    public CategoriaControlador(CategoriaServicioImpl categoriaServicio, ModelMapper modelMapper){
         this.categoriaServicio = categoriaServicio;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
-    public ResponseEntity<CustomResponse> agregarCategoria(CategoriaEntidad categoriaEntidad){
-        categoriaServicio.agregarCategoria(categoriaEntidad);
+    public ResponseEntity<CustomResponse> agregarCategoria(@RequestBody  CategoriaDto categoriaEntidad){
+        categoriaServicio.agregarCategoria(this.modelMapper.map(categoriaEntidad, CategoriaEntidad.class));
         return new ResponseEntity<>(new CustomResponse(""), HttpStatus.OK);
     }
 
